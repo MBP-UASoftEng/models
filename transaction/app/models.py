3ae70830256c1bb45682db23a7fc68c4adc1b829
brsomartins <brsomartins@gmail.com>
2015-11-08 01:42:00 -0600

    Added the relationship with employee table

--- transaction/app/models.py
+++ transaction/app/models.py
@@ -3,13 +3,41 @@ import datetime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
+class Employee(db.Model):
+
+    __tablename__ = 'employee'
+
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable = False)
+    last_name = db.Column(db.String, nullable = False)
+    employee_id = db.Column(db.Integer, unique=True, nullable = False)
+    active = db.Column(db.Boolean, nullable = False)
+    classification = db.Column(db.String, nullable = False)
+    password = db.Column(db.String, nullable = False)
+    timestamp = db.Column(db.String, nullable = False)
+    manager_id = db.Column(db.Integer, ForeignKey("employee.id"), index = True, nullable = True)
+
+    manager = relationship(lambda: Employee, remote_side=id, backref='underlings')
+
+    def __init__(self, first_name, last_name, active, classification, password, employee_id, manager):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.active = active
+        self.password = password
+        self.classification = classification
+        self.employee_id = employee_id
+        self.timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        self.manager = manager
+
+    def __repr__(self):
+        return '<First Name %r>' % self.first_name
+
 class Transaction(db.Model):
 
     __tablename__ = 'transaction'
 
     id = db.Column(db.Integer, primary_key=True)
-    # cashier_id = db.Column(db.Integer, ForeignKey("employee.id"), nullable=False)
-    cashier_id = db.Column(db.Integer, nullable=True) # Just for tests. Should interact with the "employee" table, as the line above.
+    cashier_id = db.Column(db.Integer, ForeignKey("employee.id"), nullable=False)
     amount = db.Column(db.Float(precision=2, asdecimal=False, decimal_return_scale=None), nullable=False)
     transaction_type = db.Column(db.String, nullable=False)
     parent_id = db.Column(db.Integer, ForeignKey("transaction.id"), nullable=True)
